f820bd49a167c6ab754600b7c55f94eed3a118a3
Lucy <hao.lucyy@gmail.com>
2020-12-19 23:00:22 -0700

    step 1 complete

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/shibe')
+def shibe():
+    return 'shibe'
+
+@app.route('/cat')
+def cat():
+    return 'cat'
+
+@app.route('/bird')
+def bird():
+    return 'bird'
+

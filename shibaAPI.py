b5d4e85143754849e0f8e60c52e49f5aa6933c4b
Lucy <hao.lucyy@gmail.com>
2020-12-25 12:47:39 -0700

    step 3

--- shibaAPI.py
+++ shibaAPI.py
@@ -4,8 +4,9 @@ import requests
 # if 200 is returned, this means the API can be used 
 def check_request(endpoint):
     response = requests.get(endpoint)
-    print(response.status_code)
 
+# function that returns json
 def get_request(endpoint):
     response = requests.get(endpoint)
-    # TODO: your code here
+    json = response.json()
+    return json

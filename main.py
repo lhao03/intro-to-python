cb19b7d72e12ee7ceb721069698245148d6196a4
Lucy <hao.lucyy@gmail.com>
2020-12-25 15:54:00 -0700

    styling

--- main.py
+++ main.py
@@ -16,20 +16,20 @@ def main_page():
 def shibe():
     if (check_request(ENDPOINT + "shibes")):
         response = get_request(ENDPOINT + "shibes")
-        return render_template('animal.html', name="Shiba Inu", img_url=response[0])
+        return render_template('animal.html', name="Shiba Inu", img_url=response[0], url="shibe")
     return render_template('animal.html', name="Shiba Inu")
 
 @app.route('/cat')
 def cat():
     if (check_request(ENDPOINT + "cats")):
         response = get_request(ENDPOINT + "cats")
-        return render_template('animal.html', name="Cat", img_url=response[0])
+        return render_template('animal.html', name="Cat", img_url=response[0], url="cat")
     return render_template('animal.html', name="Cat")
 
 @app.route('/bird')
 def bird():
     if (check_request(ENDPOINT + "birds")):
         response = get_request(ENDPOINT + "birds")
-        return render_template('animal.html', name="Bird", img_url=response[0])
+        return render_template('animal.html', name="Bird", img_url=response[0], url="bird")
     return render_template('animal.html', name="Bird")
 
